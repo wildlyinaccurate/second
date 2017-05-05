0241842b11c6b3416cb0f27c9f4556219ae1b9c9
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-05-05 21:46:19 +0100

    Add logging

--- second-renderer/src/fetcher.js
+++ second-renderer/src/fetcher.js
@@ -1,10 +1,14 @@
 import Promise from 'bluebird'
-import { clone, filter, join, map, mapValues, toPairs } from 'lodash/fp'
+import fp, { clone, filter, join, map, toPairs } from 'lodash/fp'
 import HttpClient from 'bbc-http-client'
+import debug from 'debug'
+
+const mapValuesWithKey = fp.mapValues.convert({ cap: false })
 
 const REFETCH_DELAY = 100
 const client = new HttpClient({ timeout: 10000 })
 const paramsToUriString = params => map(join('/'), toPairs(params))
+const log = debug('second:renderer:fetcher')
 
 export default class Fetcher {
   constructor () {
@@ -22,16 +26,18 @@ export default class Fetcher {
   // Called by clients; queues requests for fetching or returns responses if
   // the request has already been fulfilled
   request (requests) {
-    return mapValues((params, name) => {
+    return mapValuesWithKey((params, name) => {
       const key = JSON.stringify(params)
 
       if (!this.requests.hasOwnProperty(key)) {
+        log('Making request for', name)
         this.requests[key] = this.fetch(params)
       }
 
       const request = this.requests[key]
 
       if (request.isFulfilled()) {
+        log('Response received for', name)
         return request.value()
       }
 
@@ -48,6 +54,7 @@ export default class Fetcher {
         if (err) {
           reject(err)
         } else if (resp.statusCode === 200) {
+          log(`[200] ${url}`)
           resolve({
             body,
             meta: {
@@ -70,7 +77,7 @@ export default class Fetcher {
 
     delete params.data
 
-    const encodedParams = mapValues(encodeURIComponent, params)
+    const encodedParams = mapValuesWithKey(encodeURIComponent, params)
     const uriParams = paramsToUriString(encodedParams).join('/')
 
     return `https://morph.api.bbci.co.uk/data/${template}/${uriParams}`
