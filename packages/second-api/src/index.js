74b5845dc124c099422f6a697f5b06d54c4351c9
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-05-20 16:27:12 +0100

    Return a response on error instead of hanging

--- packages/second-api/src/index.js
+++ packages/second-api/src/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import debug from 'debug'
 import Renderer from 'second-renderer'
 
 import getRendererLib from './get-renderer-lib'
 import template from './template'
 
 const app = express()
+const log = debug('second:api')
 
 const DEFAULT_RENDERER_LIB = 'preact-compat'
 const [VDom, VDomServer] = getRendererLib(DEFAULT_RENDERER_LIB)
@@ -24,18 +26,20 @@ const renderModuleIntoEnvelope = (module, params) => {
   }))
 }
 
-app.get('/render/:module', (req, res) => {
+app.get('/render/:module', (req, res, next) => {
   renderModuleIntoEnvelope(req.params.module, req.query)
     .then(envelope => res.send(envelope))
+    .catch(next)
 })
 
-app.get('/preview/:module', (req, res) => {
+app.get('/preview/:module', (req, res, next) => {
   renderModuleIntoEnvelope(req.params.module, req.query)
     .then(envelope => res.render('preview', {
       head: envelope.head.join('\n'),
       bodyInline: envelope.bodyInline,
       bodyLast: envelope.bodyLast.join('\n')
     }))
+    .catch(next)
 })
 
 app.engine('html', template)
@@ -44,4 +48,4 @@ app.set('view engine', 'html')
 
 const APP_PORT = process.env.SECOND_API_PORT || 8082
 
-app.listen(APP_PORT, () => console.log(`Listening on port ${APP_PORT} (http://127.0.0.1:${APP_PORT})`))
+app.listen(APP_PORT, () => log(`Listening on port ${APP_PORT} (http://127.0.0.1:${APP_PORT})`))
