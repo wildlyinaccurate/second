0fe6fd183460cc7b00e23641dd3d4502d0385c1c
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-05-21 19:10:51 +0100

    Use request-promise instead of bbc-http-client in the fetcher

--- packages/second-fetcher/src/index.js
+++ packages/second-fetcher/src/index.js
@@ -1,19 +1,19 @@
 import Promise from 'bluebird'
 import fp, { clone, filter } from 'lodash/fp'
-import HttpClient from 'bbc-http-client'
+import request from 'request-promise'
 import debug from 'debug'
 
 const mapValuesWithKey = fp.mapValues.convert({ cap: false })
 
 const REFETCH_DELAY = 100
-const client = new HttpClient({ timeout: 10000 })
 const log = debug('second:fetcher')
 
 export default class Fetcher {
-  constructor ({ handlers = [] }) {
+  constructor ({ handlers = [], request = this._makeRequest } = {}) {
     this.requests = {}
     this.unreadResponses = false
     this.handlers = handlers
+    this.makeRequest = request
   }
 
   getUrlFor (_params) {
@@ -65,36 +65,41 @@ export default class Fetcher {
     }, requests)
   }
 
+  _makeRequest (url) {
+    return request({
+      url,
+      json: true,
+      resolveWithFullResponse: true,
+      simple: false,
+      headers: {
+        'User-Agent': 'second-fetcher'
+      }
+    })
+  }
+
   // Make an external call to fetch data
   fetch (params) {
     const url = typeof params === 'string' ? params : this.getUrlFor(params)
 
-    return new Promise((resolve, reject) => {
-      client.get({ url, json: true }, (err, resp, body) => {
-        if (err) {
-          reject(err)
-        } else if (resp.statusCode === 200) {
-          log(`[200] ${url}`)
+    return this.makeRequest(url).then(({ body, statusCode }) => {
+      if (statusCode === 200) {
+        log(`[200] ${url}`)
 
-          this.unreadResponses = true
+        this.unreadResponses = true
 
-          resolve({
-            body,
-            meta: {
-              statusCode: resp.statusCode
-            }
-          })
-        } else if (resp.statusCode === 202) {
-          log(`[202] re-fetching in ${REFETCH_DELAY}ms ${url}`)
-          resolve(
-            Promise.delay(REFETCH_DELAY).then(() => this.fetch(params))
-          )
-        } else {
-          this.unreadResponses = false
-
-          reject(new Error(`[${resp.statusCode}] Upstream request failed ${url}`))
+        return {
+          body,
+          meta: { statusCode }
         }
-      })
+      } else if (statusCode === 202) {
+        log(`[202] re-fetching in ${REFETCH_DELAY}ms ${url}`)
+
+        return Promise.delay(REFETCH_DELAY).then(() => this.fetch(params))
+      } else {
+        this.unreadResponses = false
+
+        throw new Error(`[${statusCode}] Upstream request failed ${url} / ${body}`)
+      }
     })
   }
 }
