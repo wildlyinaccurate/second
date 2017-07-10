bd9958e67ce8f8688456e97b54fca9467df52b71
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-07-10 08:44:46 +0100

    Don't use JSX in source

--- packages/second-dehydrator/src/dehydrator.js
+++ packages/second-dehydrator/src/dehydrator.js
@@ -1,4 +1,3 @@
-/** @jsx createElement */
 export default function createDehydrator (createElement) {
   return function dehydrate (Component) {
     if (typeof DISABLE_DEHYDRATOR === 'undefined') {
@@ -8,14 +7,15 @@ export default function createDehydrator (createElement) {
         const componentId = crypto.createHash('sha256').update(serialisedProps).digest('hex')
         const componentName = Component.displayName || Component.name
 
-        return (
-          <span>
-            <span data-component-name={componentName} data-hydration-id={componentId}>
-              <Component {...props} />
-            </span>
-
-            {renderSerialisedProps(componentId, serialisedProps)}
-          </span>
+        return createElement(
+          'span',
+          null,
+          createElement(
+            'span',
+            { 'data-component-name': componentName, 'data-hydration-id': componentId },
+            createElement(Component, props)
+          ),
+          renderSerialisedProps(componentId, serialisedProps)
         )
       }
     }
@@ -24,12 +24,10 @@ export default function createDehydrator (createElement) {
   }
 
   function renderSerialisedProps (componentId, serialisedProps) {
-    return (
-      <script
-        data-hydration-id={componentId}
-        type='application/hydration-data'
-        dangerouslySetInnerHTML={{ __html: serialisedProps }}
-      />
-    )
+    return createElement('script', {
+      'data-hydration-id': componentId,
+      type: 'application/hydration-data',
+      dangerouslySetInnerHTML: { __html: serialisedProps }
+    })
   }
 }
